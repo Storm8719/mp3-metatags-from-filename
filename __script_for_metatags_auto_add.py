26cf970d62a3347e1c27529a3baeac4e64138d37
Oleg Sukhanik <storm8719@gmail.com>
2025-11-01 00:54:34 +0400

    skip unexpected file formats

--- __script_for_metatags_auto_add.py
+++ __script_for_metatags_auto_add.py
@@ -7,34 +7,37 @@ from os import scandir
 from mutagen.easyid3 import EasyID3
 
 root = './'
-l_archivos = sorted([archivo.name for archivo in scandir(root) if archivo.is_file()])
-
-mutagen.File(l_archivos[1])      # U: See the tags of the data
-# print(mutagen)
-
-def edit_Media_Data():
-
-    for f in range(len(l_archivos[:-1])):                 # A: A range of all the fields exept the script
-        
-        
-        if (l_archivos[f].endswith('mp3')):
-
-            try:
-                file = EasyID3(l_archivos[f])                         # A: Capture the file to edit
-            except BaseException as err:
-                print(f"Unexpected {err=}, {type(err)=}")
-
-            filedata = l_archivos[f].replace('.mp3', '').split(' - ')
-            file['artist'] = filedata[0]
-            file['title'] = generate_title(filedata)
-            print(f+1, file['artist'], file['title'])
-            file.pprint()
-            file.save()
+l_archivos = sorted([archivo.name for archivo in scandir(root) if archivo.is_file() and archivo.name.endswith('.mp3')])
 
 def generate_title(filedata):
-    title = ''
-    for i in range(len(filedata)-1):
-        title+= filedata[i+1] + ' '
-    return title
+    # Join all parts except the first (artist)
+    return ' - '.join(filedata[1:])
 
-edit_Media_Data()
+def edit_Media_Data():
+    for idx, filename in enumerate(l_archivos, start=1):
+        try:
+            file = EasyID3(filename)
+        except mutagen.id3.ID3NoHeaderError:
+            # If no ID3 tag, create one
+            file = mutagen.File(filename, easy=True)
+            file.add_tags()
+        except Exception as err:
+            print(f"Skipping {filename} due to error: {err}")
+            continue
+
+        filedata = filename.replace('.mp3', '').split(' - ')
+        if len(filedata) < 2:
+            print(f"Skipping {filename}: unexpected filename format")
+            continue
+
+        artist = filedata[0]
+        title = generate_title(filedata)
+
+        file['artist'] = artist
+        file['title'] = title.strip()
+        file.save()
+
+        print(f"{idx}. ✅ Updated: {artist} - {title}")
+
+if __name__ == "__main__":
+    edit_Media_Data()
